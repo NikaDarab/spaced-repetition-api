4f3d1f39fca95a34b7e93a23024e8700ca772d3c
Nika Darab <nikadarab@pm.me>
2021-03-03 20:20:54 -0600

    data and test has been updated with italian words

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -17,18 +17,18 @@ VALUES
 
 INSERT INTO "language" ("id", "name", "user_id")
 VALUES
-  (1, 'French', 1);
+  (1, 'Italian', 1);
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'settimana', 'week', 2),
+  (2, 1, 'anno', 'year', 3),
+  (3, 1, 'oggi', 'today', 4),
+  (4, 1, 'domani', 'tomorrow', 5),
+  (5, 1, 'ieri', 'yesterday', 6),
+  (6, 1, 'calendario', 'calendar', 7),
+  (7, 1, 'secondo', 'second', 8),
+  (8, 1, 'ora', 'hour', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 

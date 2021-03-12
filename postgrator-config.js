716ede8dfefbe425486a09f84ba1bc294679a637
Nika Darab <nikadarab@pm.me>
2021-03-12 13:25:09 -0600

    postgrator

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,9 +4,5 @@ pg.defaults.ssl = process.env.NODE_ENV === "production";
 module.exports = {
   migrationDirectory: "migrations",
   driver: "pg",
-  host: process.env.MIGRATION_DB_HOST,
-  port: process.env.MIGRATION_DB_PORT,
-  database: process.env.MIGRATION_DB_NAME,
-  username: process.env.MIGRATION_DB_USER,
-  password: process.env.MIGRATION_DB_PASS,
+  connectionString: process.env.DATABASE_URL,
 };
